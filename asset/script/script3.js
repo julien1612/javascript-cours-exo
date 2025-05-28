a3445d8e65b8df0f56b4fe0d28aaead1d98aff62
unknown <cjulien1612@gmail.comgit config --global user.email cjulien1612@gmail.com>
2025-05-28 16:30:51 +0200

    exo2

--- asset/script/script3.js
+++ asset/script/script3.js
@@ -1,1 +1,54 @@
-console.log("bonjour")
+
+let inputElement = document.querySelector("#zoneText");
+console.log(inputElement);
+
+
+let conteneurUl = document.querySelector("#listeTodo");
+console.log(conteneurUl);
+//récupérer le conteneur -> ul
+
+
+
+let buttonAjout = document.querySelector("#ajouter");
+buttonAjout.addEventListener ('click', () => {
+    
+    console.log(inputElement.value)
+
+})
+
+    function addElement() {
+        
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    //BUT Global : créer un element html LI qu'on va mettre dans le conteneur ul
+
+    //1 : créer l'élément LI avec : createElement
+    //2: lui ajouter le contenue de la valeur de l'input avec : innerHTML
+
+    //3: ajouter un evenement au clic sur mon li
+        //4: ajouter la class qui permet de lui dire ok c'est fait : classList 
+
+
+
+
+
+
+
+    //FIN : conteneur ul -> ajouer le li : avec appendChild logique c'est conteneur -> appendChild -> li
+
