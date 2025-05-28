805a594f99e9c0317ac3a93c6843282c36301c0f
unknown <cjulien1612@gmail.comgit config --global user.email cjulien1612@gmail.com>
2025-05-28 15:03:16 +0200

    firstExo

--- asset/script/scirpt2.js
+++ asset/script/scirpt2.js
@@ -1,1 +1,35 @@
-console.log("chat");
+
+let forceUtilisateur = 10;
+let vieDuChat = 100;
+
+
+let strong = document.querySelector("#force");
+strong.innerHTML = forceUtilisateur;
+console.log(strong);
+
+let life = document.querySelector("#vie");
+life.innerHTML = vieDuChat;
+console.log(life);
+
+let chatImg = document.querySelector("img");
+chatImg.addEventListener ('click', () =>  {
+    vieDuChat -= forceUtilisateur;
+    life.innerHTML = vieDuChat;
+
+    if(vieDuChat < 0) {
+        vieDuChat = 0;
+    }
+
+    if (vieDuChat <= 0) {
+        alert("vous avez tuer le chat!");
+       chatImg.src="images/explosion.gif ";
+        setTimeout(() => {
+           chatImg.remove()
+       }, "700") 
+
+    } 
+       
+})
+
+
+
